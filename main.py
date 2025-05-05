bb036ee1c7c8a1d92b8d3268e56559d1bbdae19b
Septimastry <chishuntse.work@gmail.com>
2025-05-05 23:27:21 +0100

    Working Game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas
+import pandas as pd
 
 screen= turtle.Screen()
 screen.title("U.S. States Guesser")
@@ -9,25 +10,36 @@ turtle.shape(image)
 game_is_on = True
 ans_count = 0
 TOTAL_STATES = 50
+answered_states = []
 
 
 #create dataframe
 df = pandas.read_csv("50_states.csv")
 states = df.state
+states_list = states.to_list()
+#create function for state assignment
+def assign(guessed_state):
+    i = turtle.Turtle()
+    i.hideturtle()
+    i.penup()
+    x_coor = df[states == guessed_state].x.iloc[0]
+    y_coor = df[states == guessed_state].y.iloc[0]
+    i.goto(x_coor, y_coor)
+    i.color("black")
+    i.write(arg= f"{df[states == guessed_state].state.item()}", align="center", font=("Arial", 8, "normal"))
+
 #main loop
 while game_is_on:
     if ans_count ==0:
-        answer_state = screen.textinput("Guess the state", "What's another state's name?")
+        answer_state = screen.textinput("Guess the state", "What's another state's name?").title()
     else:
-        answer_state = screen.textinput(f"{ans_count}/{TOTAL_STATES} States Correct", "What's another state's name?")
-        edited_ans = answer_state.upper()
-        for state in states:
-            if edited_ans == state:
-                ans_count+=1
-                y = turtle.Turtle()
-                y.penup()
-                y.goto(df[states==state].x, df[states==state].y)
-
+        answer_state = screen.textinput(f"{ans_count}/{TOTAL_STATES} States Correct", "What's another state's name?").title()
+    if answer_state in states_list:
+        ans_count+= 1
+        answered_states.append(answer_state)
+        assign(answer_state)
+    if ans_count == TOTAL_STATES:
+        game_is_on = False
 
 
 
